2f44ddb403af92f33caeb9d0c619dab5addd3222
HrudaiGvs <hrudayvittal9@gmail.com>
2025-06-05 11:54:44 +0530

    Sql project.sql

--- Sql project.sql
+++ Sql project.sql
@@ -1,3 +1,18 @@
+-- Retail Sales Analysis
+-- CREATE TABLE
+CREATE TABLE retail_sales (
+transactions_id INT PRIMARY KEY,
+sale_date DATE,
+sale_time TIME,
+customer_id INT,
+gender VARCHAR(10),
+age INT,
+category VARCHAR(25),
+quantiy INT,
+price_per_unit FLOAT,
+cogs FLOAT,
+total_sale FLOAT
+);
 -- Data Cleaning
 
 select * from retail_sales
